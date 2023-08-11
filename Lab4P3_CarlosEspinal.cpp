7f6e9694c2c92f355a2f636dbf7ea2fbb125a623
Carlos Espinal <cespinalb@gmail.com>
2023-08-11 16:02:12 -0600

    commoit

--- Lab4P3_CarlosEspinal.cpp
+++ Lab4P3_CarlosEspinal.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <string>
+
+
 
 using namespace std;
 
 void menu() {
     cout << "---- MENU PRINCIPAL ---- \n\n" << "Opcion 1: Palabra mas larga \nOpcion 2: Rotacion de matriz \nOpcion 3: SALIDA \n" << "Ingrese la opcion que desea acceder: ";
+}
+
+int** produccion(int sizeM, int sizeN) {
+    int** matriz = new int* [sizeM];
+    for (size_t i = 0; i < sizeM; i++){
+        matriz[i] = new int[sizeN];
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+
+    }
+
+    return matriz;
 }
 
 int main(){
@@ -14,28 +30,65 @@ int main(){
         menu();
         cin >> opcion;
         cout << endl;
-        char oracionin[100];
-        char* chptr = strtok(oracionin, " ");
+        string oracionin;
         vector<string> tokens;
+        string token;
+        int M, N;
         switch (opcion) {
             case 1:
                 cout << "Ingrese una oracion: ";
+                cin.ignore();
+                getline(cin, oracionin);
                 cout << endl;
-                cin >> oracionin;
                 cout << oracionin;
-                while (chptr != nullptr) {
+                cout << oracionin.size();
+                cout << endl;
 
+
+                for (int i = 0; i <= oracionin.size(); i++){
+                    if (oracionin[i] == ' ') {
+                        tokens.push_back(token);
+                        cout << token;
+                        cout << endl;
+                        token.clear();
+                    }
+                    else if (i == oracionin.size() - 1) {
+                        token += oracionin[i];
+                        cout << token;
+                        tokens.push_back(token);
+                    }
+                    else {
+                        token += oracionin[i];
+                        //cout << token;
+                    }
                 }
+                for (int i = -1; i < tokens.size(); i++){
+                    cout << tokens[i] << endl;
+                }
+                    break;
+            case 2:
+                cout << "Ingrese el size de filas: ";
+                cin >> M;
+                cout << endl;
+                cout << "Ingrese el size de columnas: ";
+                cin >> N;
+
 
                 break;
-        }
-        char salir;
-        cout << "Desea seguir utilizando el programa [S/N]: ";
-        cin >> salir;
-        if (salir == 'n' || salir == 'N') {
-            opcion == 3; 
-        }
 
+        }
+        char respc;
+        cout << endl;
+        cout << "Desea continuar usando el programa(s/n)?: ";
+        cin >> respc;
+        cin.ignore();
+
+        if (respc == 's' || respc == 'S') {
+            opcion = 0;
+        }
+        else {
+            opcion = 3;
+        }
     } while (opcion != 3);
 }
 
